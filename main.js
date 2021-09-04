ebe806be8170cb260537eae090084324c1af01d5
Hanzo253 <42708236+Hanzo253@users.noreply.github.com>
2021-09-03 21:05:40 -0400

    updating the Nutrition Calculator App with the finished functions

--- main.js
+++ main.js
@@ -14,16 +14,19 @@ const calculateForm = document.getElementById("calculate");
 const calculateBtn = document.getElementById("calculate-btn");
 
 var servingsBar = null;
-var totalCalories = 0;
-var totalCarbs = 0;
-var totalCholesterol = 0;
-var totalSatFat = 0;
-var totalFat = 0;
-var totalSodium = 0;
-var totalSugars = 0;
-var totalProtein = 0;
-var totalPotassium = 0;
 
+// nutrient variables
+var calories = 0;
+var carbs = 0;
+var cholesterol = 0;
+var saturatedFat = 0;
+var fat = 0;
+var sodium = 0;
+var sugars = 0;
+var protein = 0;
+var potassium = 0;
+
+// uses the search input to make the API look for food items
 function findFood(e) {
   e.preventDefault();
 
@@ -31,6 +34,7 @@ function findFood(e) {
 
   console.log(searchTerm);
 
+  // if there is no input found in the search bar after search button has been pressed
   if (searchTerm.trim()) {
     // fetch(
     //   `https://api.nutritionix.com/v1_1/search/${searchTerm}?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=dc339553&appKey=70e3231d6bea3b462d32cb81d3dbdb7d`
@@ -54,6 +58,7 @@ function findFood(e) {
     //     }
     //   });
 
+    // using Nutritionix API to find the food items and their nutrients' data
     $.ajax({
       method: "GET",
       url: `https://trackapi.nutritionix.com/v2/search/instant?query=${searchTerm}`,
@@ -64,13 +69,27 @@ function findFood(e) {
       dataType: "json",
       success: function (result) {
         console.log(result);
-        $.map(result.branded, function (food, i) {
-          // console.log(food.nf_calories);
-          $("#results").append(`
-            <ul>
-              <li class="food" onclick="showCaloriesModal()" data-foodID="${food.nix_item_id}">${food.food_name} (${food.brand_name})</li>
-            </ul>`);
-        });
+        // when the API can not find any food related to the search input
+        if (result.branded.length === 0) {
+          alert("Food does not exist or input is not valid! Try again.");
+          $("#results-heading").hide();
+        } else {
+          // post the results if food is found
+          $.map(result.branded, function (food, i) {
+            // console.log(food.nf_calories);
+            $("#results").append(`
+              <ul>
+                <li class="food" onclick="showInfoModal()" data-foodID="${food.nix_item_id}">${food.food_name} (${food.brand_name})</li>
+              </ul>`);
+          });
+        }
+        // $.map(result.branded, function (food, i) {
+        //   // console.log(food.nf_calories);
+        //   $("#results").append(`
+        //   <ul>
+        //     <li class="food" onclick="showCaloriesModal()" data-foodID="${food.nix_item_id}">${food.food_name} (${food.brand_name})</li>
+        //   </ul>`);
+        // });
       },
       error: function ajaxError(jqXHR) {
         console.error("Error: ", jqXHR.responseText);
@@ -81,66 +100,22 @@ function findFood(e) {
     alert("No inputs detected");
   }
 
-  // var query = "3lb carrots and a chicken sandwich";
-  // $.ajax({
-  //   method: "GET",
-  //   url: "https://api.calorieninjas.com/v1/nutrition?query=" + searchTerm,
-  //   headers: { "X-Api-Key": "2PyueR1IQUQ7RVv3GbA5iQ==2MpJfXId6iEa6XTt" },
-  //   contentType: "application/json",
-  //   success: function (result) {
-  //     console.log(result.items);
-  //     $.map(result.items, function (food, i) {
-  //       console.log(food.calories);
-  //       $("#results").append(`
-  //         <h2>${food.name}</h2>
-  //         <p>Calories: ${food.calories}</p>
-  //         <p>Carbonhydrates: ${food.carbohydrates_total_g} g</p>
-  //         <p>Cholesterol: ${food.cholesterol_mg} mg</p>
-  //         <p>Saturated Fat: ${food.fat_saturated_g} g</p>
-  //         <p>Total Fat: ${food.fat_total_g} g</p>
-  //         <p>Fiber: ${food.fiber_g} g</p>
-  //         <p>Potassium: ${food.potassium_mg} mg</p>
-  //         <p>Protein: ${food.protein_g} g</p>
-  //         <p>Serving Size: ${food.serving_size_g} g</p>
-  //         <p>Sodium: ${food.sodium_mg} mg</p>
-  //         <p>Sugar: ${food.sugar_g} g</p>`);
-  //     });
-  //     if (result.items.length === 0) {
-  //       alert("Unknown food");
-  //     }
-  //   },
-  //   error: function ajaxError(jqXHR) {
-  //     console.error("Error: ", jqXHR.responseText);
-  //   },
-  // });
-
   resultsHeading.innerHTML = `<h1>Search results for: ${searchTerm}</h1>`;
   results.innerHTML = "";
 }
 
-function showCaloriesModal() {
-  // const foodTerm = search.value;
+// show the nutrients modal of the selected food item
+function showInfoModal() {
   infoModal.style.display = "block";
-  // fetch(
-  //   `https://api.nutritionix.com/v1_1/search/${foodTerm}?fields=item_name,brand_name,item_id,nf_calories&appId=dc339553&appKey=70e3231d6bea3b462d32cb81d3dbdb7d`
-  // )
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     infoModalContent.innerHTML = data.hits.map(
-  //       (hit) =>
-  //         `<span class="close">
-  //             <i class="bi bi-x-circle-fill"></i>
-  //         </span>
-  //         <h2>Food: ${hit.fields.item_name}</h2>`
-  //     );
-  //   });
 }
 
+// closes the modals whenever the close button is pressed
 function closeModal() {
   infoModal.style.display = "none";
   calculateModal.style.display = "none";
 }
 
+// uses the nix_item_id to grab nutrient data of the food item that was selected
 function getFoodItemById(foodID) {
   $.ajax({
     method: "GET",
@@ -152,6 +127,7 @@ function getFoodItemById(foodID) {
     dataType: "json",
     success: function (result) {
       console.log(result.foods);
+      // if the data of a nutrient is null, then their value is set to 0
       $.map(result.foods, function (foodItem, i) {
         // console.log(foodItem.nf_calories);
         if (foodItem.nf_potassium === null) {
@@ -185,6 +161,7 @@ function getFoodItemById(foodID) {
         if (foodItem.nf_saturated_fat === null) {
           foodItem.nf_saturated_fat = 0;
         }
+        // display the the amount of nutrients in the information modal
         $("#info-modal-content").html(`
           <span class="close" onclick="closeModal()">
             <i class="bi bi-x-circle-fill"></i>
@@ -203,7 +180,7 @@ function getFoodItemById(foodID) {
           <h2>Serving Quantity: ${foodItem.serving_qty} ${foodItem.serving_unit}</h2>
           <form id="calculate" onsubmit="calculateNutrients(event)">
             <input
-              type="text"
+              type="number"
               id="servings-bar"
             />
             <h3>
@@ -212,15 +189,17 @@ function getFoodItemById(foodID) {
             <input type="submit" value="Calculate" id="calculate-btn">
           </form>`);
         servingsBar = document.getElementById("servings-bar");
-        totalCalories = foodItem.nf_calories;
-        totalCarbs = foodItem.nf_total_carbohydrate;
-        totalCholesterol = foodItem.nf_cholesterol;
-        totalSatFat = foodItem.nf_saturated_fat;
-        totalFat = foodItem.nf_total_fat;
-        totalSodium = foodItem.nf_sodium;
-        totalSugars = foodItem.nf_sugars;
-        totalProtein = foodItem.nf_protein;
-        totalPotassium = foodItem.nf_potassium;
+
+        // set nutrient variable to the nutrient information received from the API
+        calories = foodItem.nf_calories;
+        carbs = foodItem.nf_total_carbohydrate;
+        cholesterol = foodItem.nf_cholesterol;
+        saturatedFat = foodItem.nf_saturated_fat;
+        fat = foodItem.nf_total_fat;
+        sodium = foodItem.nf_sodium;
+        sugars = foodItem.nf_sugars;
+        protein = foodItem.nf_protein;
+        potassium = foodItem.nf_potassium;
       });
     },
     error: function ajaxError(jqXHR) {
@@ -229,45 +208,47 @@ function getFoodItemById(foodID) {
   });
 }
 
+// gets the number of servings inputs to calculate the total amount of nutrients
 function calculateNutrients(event) {
   event.preventDefault();
   const numOfServings = servingsBar.value;
+  // each nutrient is multiplied by the number of servings to get their total
   if (numOfServings.trim()) {
     // console.log("total calories:", numOfServings * totalCalories);
     calculateModal.style.display = "block";
     calculateModalContent.innerHTML = `<span class="close" onclick="closeModal()">
-                                        <i class="bi bi-x-circle-fill"></i>
+                                          <i class="bi bi-x-circle-fill"></i>
                                        </span>
-                                       <h3>Total Nutrients</h3>
-                                       <ul>
-                                        <li>Total Calories: ${
-                                          numOfServings * totalCalories
-                                        }</li>
-                                        <li>Total Carbs: ${
-                                          numOfServings * totalCarbs
-                                        }g</li>
-                                        <li>Total Cholesterol: ${
-                                          numOfServings * totalCholesterol
-                                        }mg</li>
-                                        <li>Total Saturated Fat: ${
-                                          numOfServings * totalSatFat
-                                        }g</li>
-                                        <li>Total Fat: ${
-                                          numOfServings * totalFat
-                                        }g</li>
-                                        <li>Total Sodium: ${
-                                          numOfServings * totalSodium
-                                        }mg</li>
-                                        <li>Total Sugars: ${
-                                          numOfServings * totalSugars
-                                        }g</li>
-                                        <li>Total Protein: ${
-                                          numOfServings * totalProtein
-                                        }g</li>
-                                        <li>Total Potassium: ${
-                                          numOfServings * totalPotassium
-                                        }mg</li>
-                                       </ul>`;
+                                       <div id="total-text">
+                                        <h4>Total Nutrients</h4>
+                                        <p class="total">Total Calories: ${
+                                          numOfServings * calories
+                                        }</p>
+                                        <p class="total">Total Carbs: ${
+                                          numOfServings * carbs
+                                        }g</p>
+                                        <p class="total">Total Cholesterol: ${
+                                          numOfServings * cholesterol
+                                        }mg</p>
+                                        <p class="total">Total Saturated Fat: ${
+                                          numOfServings * saturatedFat
+                                        }g</p>
+                                        <p class="total">Total Fat: ${
+                                          numOfServings * fat
+                                        }g</p>
+                                        <p class="total">Total Sodium: ${
+                                          numOfServings * sodium
+                                        }mg</p>
+                                        <p class="total">Total Sugars: ${
+                                          numOfServings * sugars
+                                        }g</p>
+                                        <p class="total">Total Protein: ${
+                                          numOfServings * protein
+                                        }g</p>
+                                        <p class="total">Total Potassium: ${
+                                          numOfServings * potassium
+                                        }mg</p>
+                                       </div>`;
     infoModal.style.display = "none";
   } else {
     alert("No input detected");
@@ -276,6 +257,7 @@ function calculateNutrients(event) {
 }
 
 form.addEventListener("submit", findFood);
+// gets the unique ID of the food that was selected in the search results list
 results.addEventListener("click", (e) => {
   const foodInfo = e.path.find((foodItem) => {
     if (foodItem.classList) {
